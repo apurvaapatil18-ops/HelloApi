0f1950680be6f0c192aa2ded104c96ba0fe0e16a
Apurva Patil <apurvaapatil18@gmail.com>
2026-05-18 12:51:18 +0530

    TypeScript hello

--- obj/Debug/net10.0/HelloApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/HelloApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b83d4f314319975b00ea634c1e8b7b403facb97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d928f303e59b314ff96109bc1cda12fd23ca40")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
